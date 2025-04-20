eac868c70aeef5f2e1e0d828b0cea496d2ac5b5e
Xiaoming Wang <dawnseeker8@gmail.com>
2025-04-20 03:55:59 +0000

    mobile improvement and export fixtures

---
+++ popsicle_sale/popsicle_sale/doctype/purchase_order_template/test_purchase_order_template.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Xiaoming Wang and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPurchaseOrderTemplate(FrappeTestCase):
+	pass
---
+++ popsicle_sale/popsicle_sale/doctype/purchase_order_template_item/purchase_order_template_item.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Xiaoming Wang and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class PurchaseOrderTemplateItem(Document):
+	pass
